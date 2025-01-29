7c2247bda33cb00b2c0f37a0d490a6aaea4b9301
Shilpi <Shilpi@gmail.com>
2025-01-28 20:01:24 -0800

    Day07 commit

---
+++ Day07/com-student-app/src/main/java/com/student/app/ComStudentAppApplication.java
@@ -0,0 +1,13 @@
+package com.student.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComStudentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComStudentAppApplication.class, args);
+	}
+
+}
