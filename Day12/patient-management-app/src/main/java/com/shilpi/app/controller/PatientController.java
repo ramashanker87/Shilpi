9785c35ba19c5f0099a98c7f5f2e9aee135598c1
admin <rama@gail.com>
2025-02-09 23:33:14 -0800

    Day12 commit

---
+++ Day12/patient-management-app/src/main/java/com/shilpi/app/controller/PatientController.java
@@ -0,0 +1,44 @@
+package com.shilpi.app.controller;
+
+import com.shilpi.app.model.Patient;
+import com.shilpi.app.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/all")
+    public Map<Integer, Patient> getAllPatient(){
+        return patientService.getPatient();
+    }
+
+    @GetMapping("/age/all")
+    public void getAllPatientWithSpecificAge(@RequestParam int age){
+
+    }
+
+    @PostMapping("/save")
+    public Patient savePatient(@RequestBody Patient patient){
+        return patientService.add(patient);
+    }
+
+    @PutMapping("/update")
+    public Patient updatePatient(@RequestParam int id, @RequestParam String hospitalName){
+        return patientService.update(id, hospitalName);
+    }
+
+    @DeleteMapping("/delete")
+    public String deletePatient(@RequestParam int id){
+        return patientService.delete(id);
+    }
+
+
+
+}
---
+++ Day12/patient-management-app/src/test/java/com/shilpi/app/PatientManagementAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shilpi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class PatientManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
+
